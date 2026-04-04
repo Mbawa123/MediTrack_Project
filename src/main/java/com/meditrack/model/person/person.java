14da9429959f2453aec6fff945ebfd597c8e5355
Mbawa123 <mbawafx1@gmail.com>
2026-04-04 22:27:57 +0100

    yeah
    yeah

---
+++ src/main/java/com/meditrack/model/person/person.java
@@ -0,0 +1,41 @@
+package com.meditrack.model.person;
+
+
+public abstract class Person {
+
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+
+    // Constructor
+    public Person(String id, String name, String email, String phone) {
+        this.id    = id;
+        this.name  = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public abstract String getSummary();
+
+    // --- Getters ---
+    public String getId()    { return id; }
+    public String getName()  { return name; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+
+    // --- Setter---
+    public void setName(String name) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException("Name cannot be empty");
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        if (email == null || !email.contains("@"))
+            throw new IllegalArgumentException("Invalid email address");
+        this.email = email;
+    }
+
+    public void setPhone(String phone) { this.phone = phone; }
+}
