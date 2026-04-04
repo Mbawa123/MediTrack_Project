7b72547866760699823cfb064e57e927b5414fe3
Mbawa123 <mbawafx1@gmail.com>
2026-04-05 00:22:07 +0100

    git add . git commit -m "Week 1: model classes, OOP structure, 10 passing tests" git push

--- src/main/java/com/meditrack/Main.java
+++ src/main/java/com/meditrack/Main.java
@@ -1,7 +1,7 @@
-package com.example;
+﻿package com.meditrack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("MediTrack starting...");
     }
 }
